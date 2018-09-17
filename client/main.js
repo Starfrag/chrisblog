26d41cf07c15850c90c90ecc04fadac4c306d408
Starfrag <Christopher.brizais@gmail.com>
2018-09-17 17:56:20 +0200

    Installed react and react-dom

--- client/main.js
+++ client/main.js
@@ -1,6 +1,12 @@
 import { meteor, Meteor } from 'meteor/meteor'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 
 Meteor.startup(() => {
+    ReactDOM.render(
+        <h1>Hello</h1>,
+        document.getElementById('root')
+    )
     console.log('CLIENT: LAUNCH')
 })
