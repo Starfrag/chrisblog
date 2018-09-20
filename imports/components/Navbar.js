74248bb29683761783507569ced8b429b40285f3
Christopher <Christopher.brizais@gmail.com>
2018-09-20 11:51:07 +0200

    Sandbox for navbar

--- imports/components/Navbar.js
+++ imports/components/Navbar.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react'
+import { Menu } from 'semantic-ui-react'
 
 export default class Navbar extends Component {
 
-    state = {
-
-    }
-
+    state = { activeItem: 'home' }
+  
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  
     render() {
-        const {color, backgroundColor, height} = this.props
-        return(
-            <div className="navbar" style={{color, backgroundColor, height}}>
-                <p style={{color}}>NAVBAR</p>
-            </div>
-        )
-    }
+      const { activeItem } = this.state
+  
+      return (
+        <div className="navbar">
+          <Menu pointing secondary>
+            <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+            <Menu.Item
+              name='messages'
+              active={activeItem === 'messages'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name='friends'
+              active={activeItem === 'friends'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Menu position='right'>
+              <Menu.Item
+                name='logout'
+                active={activeItem === 'logout'}
+                onClick={this.handleItemClick}
+              />
+            </Menu.Menu>
+          </Menu>
 
-}
+        </div>
+      )
+    }
+  }
--- imports/pages/Landing.js
+++ imports/pages/Landing.js
@@ -1,43 +1,17 @@
 import React, { Component } from 'react'
 import Navbar from '/imports/components/Navbar'
-import { Button, Icon } from 'semantic-ui-react'
 
 export default class Landing extends Component {
 
     state = {
-        title: "Chris Blog",
-        description: "Le Blog de Chris",
-        age: 27
+
     }
 
-    birthday = () => this.setState({age: this.state.age + 1})
+    render() {
 
-    handleChange = (e) => this.setState({
-        [e.target.name]: e.target.type == "number" ? parseFloat(e.target.value) : e.target.value
-    })
-
-    render(){
-        const { title, description, age, navbar_color, navbar_background, navbar_height } = this.state
-        const { color = "red"} = this.props
-
-        return(
+        return (
             <div>
-                <Navbar 
-                    color={navbar_color} 
-                    backgroundColor={navbar_background} 
-                    height={navbar_height} />
-                <h1 style={{color: color}}>{title}</h1>
-                <p>{description}</p>
-                <p>Chris est un jeune développeur de {age} ans</p>
-                <Button inverted color='orange' onClick={this.birthday}><Icon name="user"/>Joyeux anniversaire !</Button>
-                <form>
-                    <input type="text" name="title" value={title} onChange={this.handleChange} />
-                    <input type="text" name="description" value={description} onChange={this.handleChange} />
-                    <input type="number" name="age" value={age} onChange={this.handleChange} />
-                    <input placeholder="navbar_color" type="text" name="navbar_color" value={navbar_color} onChange={this.handleChange} />
-                    <input placeholder="navbar_background" type="text" name="navbar_background" value={navbar_background} onChange={this.handleChange} />
-                    <input placeholder="navbar_height" type="text" name="navbar_height" value={navbar_height} onChange={this.handleChange} />
-                </form>
+                <Navbar />
             </div>
         )
     }
