717d9899939f80cd6d3e5cf26ab5b6ef15eae8a3
Christopher <Christopher.brizais@gmail.com>
2018-09-21 15:04:43 +0200

    Changed Navbar interface with Dropdowns

--- imports/components/Navbar.js
+++ imports/components/Navbar.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-import { Menu, Segment } from 'semantic-ui-react'
+import { Menu, Segment, Dropdown } from 'semantic-ui-react'
 
 export default class Navbar extends Component {
 
+  // TODO Modification du bouton selon le status connecté/déconnecté
+
   state = {
-    activeItem: 'home'
+    activeItem: 'Accueil'
   }
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
@@ -16,27 +18,45 @@ export default class Navbar extends Component {
       <nav>
         <Segment inverted className='navbar'>
           <Menu inverted pointing secondary>
+
             <Menu.Item
-              name='home'
-              active={activeItem === 'home'}
-              onClick={this.handleItemClick} />
-            <Menu.Item
-              name='messages'
-              active={activeItem === 'messages'}
+              name='Accueil'
+              active={activeItem === 'Accueil'}
               onClick={this.handleItemClick}
             />
-            <Menu.Item
-              name='friends'
-              active={activeItem === 'friends'}
+
+            {/* TODO - Ajouter handleItemClick sur les sous-menus/Dropdowns */}
+
+            <Dropdown text='Association' pointing className='link item'
+              name='Association'
+              active={activeItem === 'Association'}
               onClick={this.handleItemClick}
-            />
+            >
+              <Dropdown.Menu>
+                <Dropdown.Item>Trouver une association</Dropdown.Item>
+                <Dropdown.Item>Hidden(Ajouter mon association)</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+
+            <Dropdown text='Mon Compte' pointing className='link item'
+              name='Mon Compte'
+              active={activeItem === 'Mon Compte'}
+              onClick={this.handleItemClick}
+            >
+              <Dropdown.Menu>
+                <Dropdown.Item>Hidden(Consulter mon profil)</Dropdown.Item>
+                <Dropdown.Item>Créer un compte</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+
             <Menu.Menu position='right'>
               <Menu.Item
-                name='log out'
-                active={activeItem === 'log out'}
+                name='Se déconnecter'
+                active={activeItem === 'Se déconnecter'}
                 onClick={this.handleItemClick}
               />
             </Menu.Menu>
+
           </Menu>
         </Segment>
       </nav>
