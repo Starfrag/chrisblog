2af9b4b7a6898dd6968f78be11ae7857d0598a37
Christopher <Christopher.brizais@gmail.com>
2018-09-26 14:03:38 +0200

    Add Content to Landing

---
+++ imports/components/LandingContent.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Container, Grid } from 'semantic-ui-react'
+
+const LandingContent = () => (
+    <Container textAlign='center' className='LandingContent_1'>
+        <Grid divided='vertically'>
+            <Grid.Row columns={1}>
+                <Grid.Column>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer placerat congue justo vitae viverra. Praesent vehicula nisl purus,
+                        in porttitor ante pretium id. Quisque at laoreet urna. Aliquam varius elit est, id commodo enim tincidunt at. Nam gravida consectetur venenatis.
+                        Nullam sit amet dignissim sem. Ut eget diam at arcu maximus pharetra sit amet non justo. Duis ac nunc nisi.
+                        </p>
+                </Grid.Column>
+            </Grid.Row>
+
+            <Grid.Row columns={1}>
+                <Grid.Column>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer placerat congue justo vitae viverra. Praesent vehicula nisl purus,
+                            in porttitor ante pretium id. Quisque at laoreet urna. Aliquam varius elit est, id commodo enim tincidunt at. Nam gravida consectetur venenatis.
+                            Nullam sit amet dignissim sem. Ut eget diam at arcu maximus pharetra sit amet non justo. Duis ac nunc nisi.
+                        </p>
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
+    </Container>
+)
+
+export default LandingContent
--- imports/pages/Landing.js
+++ imports/pages/Landing.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Navbar from '/imports/components/Navbar'
 import Header from '/imports/components/Header'
+import LandingContent from '/imports/components/LandingContent'
 import Footer from '/imports/components/Footer'
 
 export default class Landing extends Component {
@@ -15,6 +16,7 @@ export default class Landing extends Component {
             <div>
                 <Navbar />
                 <Header />
+                <LandingContent />
                 <Footer />
             </div>
         )
