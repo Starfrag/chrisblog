c5346ba18fcaa505d3b58bc8b1d4b42e85a79ac0
Jimmy <jeremy.simonklein@gmail.com>
2018-10-13 18:47:35 +0200

    Added methods calls and publications in Search page

--- imports/pages/Search.js
+++ imports/pages/Search.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import { PostForm } from '/imports/components'
 import { Container } from 'semantic-ui-react'
+import { withTracker } from 'meteor/react-meteor-data'
+import Posts from '/imports/api/posts/posts'
 
-export default class Search extends Component {
+class Search extends Component {
 
     state = {
 
@@ -10,13 +12,39 @@ export default class Search extends Component {
 
     render() {
 
+        const {posts, loading} = this.props
+
+        console.log("posts", posts)
+
         return (
             <div>
                 <Container>
                     <h1>Dans l'attente de la fin du site, un formulaire pour créer des posts ! Batard !</h1>
                     <PostForm />
+                    {!loading &&
+                        <div>
+                            {posts.map(p => {
+                                return(
+                                    <div>
+                                        <h2>{p.title}</h2>
+                                        <p>{p.content}</p>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    }
                 </Container>
             </div>
         )
     }
 }
+
+export default SearchContainer = withTracker(() => {
+    const publication = Meteor.subscribe('posts.all')
+    const loading = !publication.ready()
+    const posts = Posts.find({}).fetch()
+    return {
+        loading,
+        posts
+    }
+})(Search)
